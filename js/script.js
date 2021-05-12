cffa24ca28b5e91285d8d0f239172735be61e5cf
wnsdnn <a24369029@gmail.com>
2021-05-12 19:29:33 +0900

    wnsdnn

--- js/script.js
+++ js/script.js
@@ -1,43 +1,37 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
 const tools = document.querySelector(".tools");
+const resetBtn = document.querySelector(".resetBtn");
+const size = document.querySelector(".size input");
 
-const high = function(target){
-    target.style.background = "#eee";
-}
-const removeHigh = function(){
-    // [...tools.children].forEach((data) => {
-    //     console.log(data)
-    //     data.style.background = "none"
-    // });
-}
-tools.addEventListener("click", function({target}){
-    removeHigh();
-    if(target.parentNode.classList.contains("pencil")){
-        high(target);
-
-        return false;
-    }
-    if(target.parentNode.classList.contains("rectangle")){
-        high(target);
-
-        return false;
-    }
-    if(target.parentNode.classList.contains("circle")){
-        high(target);
-
-        return false;
-    }
-})
+const xx = document.querySelector(".x");
+const yy = document.querySelector(".y");
 
 let pos = {
     drawable: false,
     x: -1,
     y: -1
 };
-let canvas = document.querySelector(".canvas");
-let ctx = canvas.getContext("2d");
 
+// 하이라이트
+const highlight = function(target){
+    target.parentNode.style.background = "#eee";
+}
 
-const listener = function(event){
+const reset = function(){
+    // 하이라이트 해제
+    [...tools.children].forEach((data) => {
+        data.style.background = "#fff";
+    });
+
+    // 실행중인 canvas이벤트 중지
+    canvas.removeEventListener("mousedown", drawListener);
+    canvas.removeEventListener("mousemove", drawListener);
+    canvas.removeEventListener("mouseup", drawListener);
+    canvas.removeEventListener("mouseout", drawListener);
+}
+
+const drawListener = function(event){
     switch(event.type){
         case "mousedown":
             initDraw(event);
@@ -58,14 +52,14 @@ const listener = function(event){
 const initDraw = function(event){
     ctx.beginPath();
     pos.drawable = true;
-    var coors = getPosition(event);
+    let coors = getPosition(event);
     pos.X = coors.X;
     pos.Y = coors.Y;
     ctx.moveTo(pos.X, pos.Y);
 }
  
 const draw =function(event){
-    var coors = getPosition(event);
+    let coors = getPosition(event);
     ctx.lineTo(coors.X, coors.Y);
     pos.X = coors.X;
     pos.Y = coors.Y;
@@ -79,12 +73,64 @@ const finishDraw =function(){
 }
  
 const getPosition = function(event){
-    var x = event.pageX - canvas.offsetLeft;
-    var y = event.pageY - canvas.offsetTop;
+    let x = event.pageX - canvas.offsetLeft;
+    let y = event.pageY - canvas.offsetTop;
     return {X: x, Y: y};
 }
 
-canvas.addEventListener("mousedown", listener);
-canvas.addEventListener("mousemove", listener);
-canvas.addEventListener("mouseup", listener);
-canvas.addEventListener("mouseout", listener);
+tools.addEventListener("click", function({target}){
+    if(target.parentNode.classList.contains("pencil")){
+        reset();
+        highlight(target);
+
+        canvas.addEventListener("mousedown", drawListener);
+        canvas.addEventListener("mousemove", drawListener);
+        canvas.addEventListener("mouseup", drawListener);
+        canvas.addEventListener("mouseout", drawListener);
+        
+        return false;
+    }
+    if(target.parentNode.classList.contains("eraser")){
+        reset();
+        highlight(target);
+
+        return false;
+    }
+    if(target.parentNode.classList.contains("rectangle")){
+        reset();
+        highlight(target);
+
+        return false;
+    }
+    if(target.parentNode.classList.contains("circle")){
+        reset();
+        highlight(target);
+
+        return false;
+    }
+});
+
+canvas.addEventListener("mousemove", function(event){
+    let x = event.pageX - canvas.offsetLeft;
+    let y = event.pageY - canvas.offsetTop;
+    // pageX 마우스의 x축
+    // pageY 마우스의 y축
+    // offsetLeft 캔버스가 왼쪽벽에서 떨어져있는 길이?
+    // offsetTop 캔버스가 윗벽에서 떨어져있는 길이?
+    xx.value = `x: ${x}`;
+    yy.value = `y: ${y}`;
+})
+
+resetBtn.addEventListener("click", function(){
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(1)
+})
+
+size.addEventListener("keydown", function({keyCode}){
+    // size.value = size.value.replace(/[^0-9]/g, "");
+    if(keyCode === 13){
+        console.log(size.value)
+    }
+    // console.log(keyCode)
+})
