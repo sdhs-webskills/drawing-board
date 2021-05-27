52e9e823679e4c0e60455d981156f88973e70266
wnsdnn <a24369029@gmail.com>
2021-05-27 19:39:07 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -12,25 +12,45 @@ const downloadBtn = document.querySelector(".save #download");
 const title = document.querySelector(".save .title")
 
 let drawable = false;
+let shapes = undefined;
+
+let rec = {X: 0, Y: 0}
+let pos = {X: 0, Y: 0}
 
 const reset = function () {
     [...tools.children].forEach((data) => data.style.background = "#fff");
     ctx.fillStyle = "#000000";
     ctx.strokeStyle = "#000000";
     colorBtn.value = "#000000"
-    size.value = 2;
+    size.value = 3;
     ctx.lineWidth = 1;
+    shapes = undefined;
 
     canvas.removeEventListener("mousedown", drawListener);
-    canvas.removeEventListener("mousemove", drawListener);
+    window.removeEventListener("mousemove", drawListener);
     window.removeEventListener("mouseup", drawListener);
     canvas.removeEventListener("mouseenter", drawListener);
+
+    canvas.removeEventListener("mousedown", shapesListener);
+    window.removeEventListener("mouseup", shapesListener);
+    window.removeEventListener("mousemove", shapesListener);
 };
 
 const saveReset = function(){
     photoBox.innerHTML = '';
     title.value ='';
 }
+
+const initDraw = function (e) {
+    ctx.beginPath();
+    drawable = true;
+    ctx.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+};
+
+const draw = function (e) {
+    ctx.lineTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+    ctx.stroke();
+};
 
 const drawListener = function (e) {
     switch (e.type) {
@@ -50,15 +70,51 @@ const drawListener = function (e) {
     }
 };
 
-const initDraw = function (e) {
+const downShapes = function (e) {
     ctx.beginPath();
     drawable = true;
-    ctx.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+    rec.X = e.pageX - canvas.offsetLeft
+    rec.Y = e.pageY - canvas.offsetTop
+    console.log(rec.X, rec.Y);
 };
 
-const draw = function (e) {
-    ctx.lineTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
-    ctx.stroke();
+
+const moveShapes = function(e){
+    if(pos.X !== 0 && pos.Y !== 0)
+        if(shapes === "rectangle")
+            ctx.clearRect(rec.X, rec.Y, pos.X - rec.X, pos.Y - rec.Y);
+    pos = {
+        X: e.pageX - canvas.offsetLeft,
+        Y: e.pageY - canvas.offsetTop
+    }
+    if(shapes === "rectangle"){
+        ctx.fillRect(rec.X, rec.Y, pos.X - rec.X, pos.Y - rec.Y);
+    } else {
+        ctx.arc((pos.X - rec.X), (pos.Y - rec.Y), (pos.X - rec.X) / 2, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+}
+
+const upShapes = function(){
+    ctx.beginPath();
+    pos = {X: 0, Y: 0}
+}
+
+
+const shapesListener = function (e) {
+    switch (e.type) {
+        case "mousedown":
+            downShapes(e);
+            break;
+        case "mousemove":
+            if(drawable)
+                moveShapes(e);
+            break;
+        case "mouseup":
+            upShapes();
+            drawable = false
+            break;
+    }
 };
 
 reset();
@@ -69,7 +125,7 @@ tools.addEventListener("click", function ({ target }) {
 
     switch (target.parentNode.classList[0]) {
         case "pencil":
-            canvas.addEventListener("mousemove", drawListener);
+            window.addEventListener("mousemove", drawListener);
             canvas.addEventListener("mouseleave", drawListener);
             canvas.addEventListener("mousedown", drawListener);
             window.addEventListener("mouseup", drawListener);
@@ -78,25 +134,31 @@ tools.addEventListener("click", function ({ target }) {
 
             break;
         case "rectangle":
-
+            shapes = "rectangle";
+            canvas.addEventListener("mousedown", shapesListener);
+            window.addEventListener("mouseup", shapesListener);
+            window.addEventListener("mousemove", shapesListener);
             break;
         case "circle":
-
+            shapes = "circle";
+            canvas.addEventListener("mousedown", shapesListener);
+            window.addEventListener("mouseup", shapesListener);
+            window.addEventListener("mousemove", shapesListener);
             break;
     };
 });
 
 
-colorBtn.addEventListener("change", function () {
+colorBtn.addEventListener("change", function (){
     ctx.fillStyle = colorBtn.value;
     ctx.strokeStyle = colorBtn.value;
 });
 
-size.addEventListener("change", function() {
+size.addEventListener("change", function(){
     ctx.lineWidth = size.value;
 });
 
-canvas.addEventListener('contextmenu', function (e) {
+canvas.addEventListener('contextmenu', function (e){
     e.preventDefault();
     savePopup.classList.toggle("none")
     
