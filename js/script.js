2c8d1a500e22be3165c58cc0c0ea9cc197c37c1d
yeongyu05 <kimyeongyu005@gmail.com>
2022-12-09 00:03:36 +0900

    upload

--- js/script.js
+++ js/script.js
@@ -3,23 +3,41 @@ const ctx = canvas.getContext('2d');
 width = document.body.clientWidth;
 height = document.body.clientHeight;
 canvas.width = width;
-canvas.height = height;
+canvas.height = height - 100;
+
+let isDraw = false;
+
+const data = {
+    color: '#000000',
+    thickness: 16,
+    fontSize: 16,
+    fontFamily: 'sans-serif',
+}
 
 const handleCanvasMousedown = ({offsetX, offsetY}) => {
-    console.log(offsetX, offsetY);
-    ctx.strokeStyle = '#000';
+    isDraw = true;
+    ctx.strokeStyle = data.color;
+    ctx.lineWidth = data.thickness;
+    ctx.lineJoin = 'round';
     ctx.beginPath();
     ctx.moveTo(offsetX, offsetY);
 }
 
 const handleCanvasMousemove = ({offsetX, offsetY}) => {
+    if(!isDraw) return;
     ctx.lineTo(offsetX, offsetY);
+    ctx.stroke();
+}
+
+const handleCanvasMouseleave = () => {
+    ctx.beginPath();
 }
 
 const handleCanvasMouseUp = () => {
-    ctx.stroke();
+    isDraw = false;
 }
 
 canvas.addEventListener('mousedown', handleCanvasMousedown);
 canvas.addEventListener('mousemove', handleCanvasMousemove);
+canvas.addEventListener('mouseleave', handleCanvasMouseleave);
 canvas.addEventListener('mouseup', handleCanvasMouseUp);
